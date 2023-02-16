ca3fbd1c8c16168aa103cbb48c50ae7e9d4311b7
Langello <gastonchavo@gmail.com>
2023-02-16 18:48:45 -0300

    ahora cuando cambias de pagina el titulo y el icono cambian

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,14 @@
+let titulo = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Vuelve";
+    document.getElementById('iconoTitulo').href = "triste.ico";
+});
+window.addEventListener("focus" , () => {
+    document.title=titulo;
+    document.getElementById('iconoTitulo').href = "contento.ico";
+
+})
+
 let educacionVisible = false;
 function mostrarOcultarEducacion() {
 
