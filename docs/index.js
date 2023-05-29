cdcd0b435ca4579403685f37de509b80c201774a
Langello <gastonchavo@gmail.com>
2023-05-29 01:04:46 -0300

    cambie que los iconos de las flechas se cambien cuando le s haces click, entre otras cosas

--- docs/index.js
+++ docs/index.js
@@ -11,25 +11,33 @@ window.addEventListener("focus" , () => {
 
 let educacionVisible = false;
 function mostrarOcultarEducacion() {
-
+    const iconEducacion = document.getElementById("iconEducacion")
     educacionVisible == false? (
-        document.getElementById("educacion").style.display = "inline",
-        educacionVisible = true
+        document.getElementById("educacionArticle").style.display = "inline",
+        educacionVisible = true,
+        iconEducacion.classList.remove("fa-arrow-down"),
+        iconEducacion.classList.add("fa-arrow-up")
     ):(
-        document.getElementById("educacion").style.display = "none",
-        educacionVisible = false
+        document.getElementById("educacionArticle").style.display = "none",
+        educacionVisible = false,
+        iconEducacion.classList.remove("fa-arrow-up"),
+        iconEducacion.classList.add("fa-arrow-down")
     );
 }
 
 let experienciaLaboralVisible = false;
 function mostrarOcultarExperienciaLaboral() {
-
+    const iconExperienciaLboral= document.getElementById("iconExperienciaLaboral")
     experienciaLaboralVisible == false? (
-        document.getElementById("experienciaLaboral").style.display= "inline",
-        experienciaLaboralVisible = true
+        document.getElementById("experienciaLaboralArticle").style.display= "inline",
+        experienciaLaboralVisible = true,
+        iconExperienciaLboral.classList.remove("fa-arrow-down"),
+        iconExperienciaLboral.classList.add("fa-arrow-up")
     ):(
-        document.getElementById("experienciaLaboral").style.display = "none",
-        experienciaLaboralVisible = false
+        document.getElementById("experienciaLaboralArticle").style.display = "none",
+        experienciaLaboralVisible = false,
+        iconExperienciaLboral.classList.remove("fa-arrow-up"),
+        iconExperienciaLboral.classList.add("fa-arrow-down")
     );
 }
 
@@ -37,12 +45,32 @@ function mostrarOcultarExperienciaLaboral() {
 
 let idiomasVisible = false;
 function mostrarOcultarIdiomas() {
-
+    const iconIdiomas= document.getElementById("iconIdiomas")
     idiomasVisible == false? (
-        document.getElementById("idiomas").style.display= "inline",
-        idiomasVisible = true
+        document.getElementById("idiomasArticle").style.display= "inline",
+        idiomasVisible = true,
+        iconIdiomas.classList.remove("fa-arrow-down"),
+        iconIdiomas.classList.add("fa-arrow-up")
     ):(
-        document.getElementById("idiomas").style.display = "none",
-        idiomasVisible = false
+        document.getElementById("idiomasArticle").style.display = "none",
+        idiomasVisible = false,
+        iconIdiomas.classList.remove("fa-arrow-up"),
+        iconIdiomas.classList.add("fa-arrow-down")
+    );
+}
+
+let sobreMiVisible = false;
+function mostrarOcultarSobreMi() {
+    const iconSobreMi= document.getElementById("iconSobreMi")
+    sobreMiVisible == false? (
+        document.getElementById("sobreMiArticle").style.display= "inline",
+        sobreMiVisible = true,
+        iconSobreMi.classList.remove("fa-arrow-down"),
+        iconSobreMi.classList.add("fa-arrow-up")
+    ):(
+        document.getElementById("sobreMiArticle").style.display = "none",
+        sobreMiVisible = false,
+        iconSobreMi.classList.remove("fa-arrow-up"),
+        iconSobreMi.classList.add("fa-arrow-down")
     );
 }
