7dd262f56032e7e3d8e5128f8042012336028b3d
Langello <gastonchavo@gmail.com>
2023-05-31 11:19:07 -0300

    mejore el codigo de javascript, lo hice mas corto

--- docs/index.js
+++ docs/index.js
@@ -9,68 +9,26 @@ window.addEventListener("focus" , () => {
 
 })
 
-let educacionVisible = false;
-function mostrarOcultarEducacion() {
-    const iconEducacion = document.getElementById("iconEducacion")
-    educacionVisible == false? (
-        document.getElementById("educacionArticle").style.display = "inline",
-        educacionVisible = true,
-        iconEducacion.classList.remove("fa-arrow-down"),
-        iconEducacion.classList.add("fa-arrow-up")
-    ):(
-        document.getElementById("educacionArticle").style.display = "none",
-        educacionVisible = false,
-        iconEducacion.classList.remove("fa-arrow-up"),
-        iconEducacion.classList.add("fa-arrow-down")
-    );
-}
-
-let experienciaLaboralVisible = false;
-function mostrarOcultarExperienciaLaboral() {
-    const iconExperienciaLboral= document.getElementById("iconExperienciaLaboral")
-    experienciaLaboralVisible == false? (
-        document.getElementById("experienciaLaboralArticle").style.display= "inline",
-        experienciaLaboralVisible = true,
-        iconExperienciaLboral.classList.remove("fa-arrow-down"),
-        iconExperienciaLboral.classList.add("fa-arrow-up")
-    ):(
-        document.getElementById("experienciaLaboralArticle").style.display = "none",
-        experienciaLaboralVisible = false,
-        iconExperienciaLboral.classList.remove("fa-arrow-up"),
-        iconExperienciaLboral.classList.add("fa-arrow-down")
-    );
-}
-
-
-
-let idiomasVisible = false;
-function mostrarOcultarIdiomas() {
-    const iconIdiomas= document.getElementById("iconIdiomas")
-    idiomasVisible == false? (
-        document.getElementById("idiomasArticle").style.display= "inline",
-        idiomasVisible = true,
-        iconIdiomas.classList.remove("fa-arrow-down"),
-        iconIdiomas.classList.add("fa-arrow-up")
-    ):(
-        document.getElementById("idiomasArticle").style.display = "none",
-        idiomasVisible = false,
-        iconIdiomas.classList.remove("fa-arrow-up"),
-        iconIdiomas.classList.add("fa-arrow-down")
-    );
-}
-
-let sobreMiVisible = false;
-function mostrarOcultarSobreMi() {
-    const iconSobreMi= document.getElementById("iconSobreMi")
-    sobreMiVisible == false? (
-        document.getElementById("sobreMiArticle").style.display= "inline",
-        sobreMiVisible = true,
-        iconSobreMi.classList.remove("fa-arrow-down"),
-        iconSobreMi.classList.add("fa-arrow-up")
-    ):(
-        document.getElementById("sobreMiArticle").style.display = "none",
-        sobreMiVisible = false,
-        iconSobreMi.classList.remove("fa-arrow-up"),
-        iconSobreMi.classList.add("fa-arrow-down")
-    );
-}
+let articulosVisibles = {
+    educacion: false,
+    experienciaLaboral: false,
+    idiomas: false,
+    sobreMi: false
+  };
+  
+function mostrarOcultarArticulo(articulo) {
+    const icon = document.getElementById(`icon${articulo}`);
+    const article = document.getElementById(`${articulo}Article`);
+  
+    articulosVisibles[articulo] = !articulosVisibles[articulo];
+  
+    if (articulosVisibles[articulo]) {
+      article.style.display = "inline";
+      icon.classList.remove("fa-arrow-down");
+      icon.classList.add("fa-arrow-up");
+    } else {
+      article.style.display = "none";
+      icon.classList.remove("fa-arrow-up");
+      icon.classList.add("fa-arrow-down");
+    }
+  }
